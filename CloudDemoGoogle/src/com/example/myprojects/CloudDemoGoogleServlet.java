aa8e1e85362096016929f767dd9dec98c2bf5a80
DARWIN <DARWIN@user-PC>
2015-08-31 20:01:52 -0500

    Servlet java3

--- CloudDemoGoogle/src/com/example/myprojects/CloudDemoGoogleServlet.java
+++ CloudDemoGoogle/src/com/example/myprojects/CloudDemoGoogleServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class CloudDemoServlet extends HttpServlet {
+public class CloudDemoGoogleServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world!!!");
+		for (int i = 1; i < 10; i++) {
+			resp.getWriter().println("Hello, world");
+		}
 	}
 }
